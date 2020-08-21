ebc6d0f358d19618d2a162316aef9b9895d5a92b
gabenn <52204063+gabenn@users.noreply.github.com>
2020-08-21 14:18:33 +0200

    push

--- creatingElements.js
+++ creatingElements.js
@@ -1,110 +1,75 @@
-//modyfikator gry
-const playerZone=document.getElementById("playerZone");
-const computerZone= document.getElementById("computerZone");
-const gameZone=document.getElementById("gameZone");
+const playerZone = document.getElementById("playerZone");
+const computerZone = document.getElementById("computerZone");
+const gameZone = document.getElementById("gameZone");
 
-const playerSpan=document.getElementById("playerSpan");
-const computerSpan=document.getElementById("computerSpan");    
+const playerSpan = document.getElementById("playerSpan");
+const computerSpan = document.getElementById("computerSpan");
 
-sizeChoose= prompt("Wybierz ilość pól \n 1 = 4X4, 2 = 6X6, 3 = 8X8");
-//console.log(sizeChoose)
-// upewnienie się że gracz wybierze dobrze
+sizeChoose = prompt("Wybierz ilość pól. Select the number of fields  \n1 = 4X4, 2 = 6X6, 3 = 8X8");
 
-playerSpan.innerHTML="STREFA GRACZA"
-computerSpan.innerHTML="STREFA KOMPUTERA"
-
-while(parseInt(sizeChoose)<1 || parseInt(sizeChoose)>3 || sizeChoose==null || sizeChoose=="")
-{
-    sizeChoose= prompt("Podałeś / łaś złą wartość \n 1 = 4X4, 2 = 6X6, 3 = 8X8");
+while (parseInt(sizeChoose) < 1 || parseInt(sizeChoose) > 3 || sizeChoose == null || sizeChoose == "") {
+    sizeChoose = prompt("Podałeś/łaś złą wartość. Enter a valid value please. \n 1 = 4X4, 2 = 6X6, 3 = 8X8");
 }
 sizeChoose = parseInt(sizeChoose);
-//switch tworzący odpowiednią ilość statków
-switch(sizeChoose)
-{
-    case 1:
-    {
-        for(let i=1;i<=16;i++)
-        {
+
+switch (sizeChoose) {
+    case 1: {
+        for (let i = 1; i <= 16; i++) {
             let input = document.createElement("input");
-            input.type="button";
-            // input.value=i;
-            input.className="player";
-            input.id="p"+i;
+            input.type = "button";
+            input.id = "p" + i;
+            input.className = "player";
             playerZone.appendChild(input);
         }
-        for(let i=1;i<=16;i++)
-        {
+        for (let i = 1; i <= 16; i++) {
             let input = document.createElement("input");
-            input.type="button";
-            // input.value=i;
-            input.className="computer";
-            input.id="c"+i;
-            input.setAttribute('onclick','game('+i+")");
+            input.type = "button";
+            input.id = "c" + i;
+            input.className = "computer";
+            input.setAttribute('onclick', 'game(' + i + ")");
             computerZone.appendChild(input);
-            }
-        playerZone.style.width=150;
-        playerZone.style.height=150;
-        computerZone.style.width=150;
-        computerZone.style.height=150;
-        gameZone.style.width=200;
-        gameZone.style.height=300;
+        }
+        playerZone.style.width = 150;
+        computerZone.style.width = 150;
     }
     break;
-    case 2:
-    {
-        for(let i=1;i<=36;i++)
-        {
+case 2: {
+    for (let i = 1; i <= 36; i++) {
         let input = document.createElement("input");
-        input.type="button";
-        // input.value=i;
-        input.className="player";
-        input.id="p"+i;
+        input.type = "button";
+        input.id = "p" + i;
+        input.className = "player";
         playerZone.appendChild(input);
-        }
-        for(let i=1;i<=36;i++)
-        {
-            let input = document.createElement("input");
-            input.type="button";
-            // input.value=i;
-            input.className="computer";
-            input.id="c"+i;
-            input.setAttribute('onclick','game('+i+")");
-            computerZone.appendChild(input);
-        }
-        playerZone.style.width=210;
-        playerZone.style.height=210;
-        computerZone.style.width=210;
-        computerZone.style.height=210;
-        gameZone.style.width=220;
-        gameZone.style.height=430;
     }
-    break;
-    case 3:
-    {                    
-        for(let i=1;i<=64;i++)
-        {
-            let input = document.createElement("input");
-            input.type="button";
-            // input.value=i;
-            input.className="player";
-            input.id="p"+i;
-            playerZone.appendChild(input);
-        }
-        for(let i=1;i<=64;i++)
-        {
-            let input = document.createElement("input");
-            input.type="button";
-            // input.value=i;
-            input.className="computer";
-            input.id="c"+i;
-            input.setAttribute('onclick','game('+i+")");
-            computerZone.appendChild(input);
-        }
-        playerZone.style.width=270;
-        playerZone.style.height=270;
-        computerZone.style.width=270;
-        computerZone.style.height=270;
-        gameZone.style.width=280;
-        gameZone.style.height=550;
+    for (let i = 1; i <= 36; i++) {
+        let input = document.createElement("input");
+        input.type = "button";
+        input.id = "c" + i;
+        input.className = "computer";
+        input.setAttribute('onclick', 'game(' + i + ")");
+        computerZone.appendChild(input);
     }
+    playerZone.style.width = 210;
+    computerZone.style.width = 210;
+}
+break;
+case 3: {
+    for (let i = 1; i <= 64; i++) {
+        let input = document.createElement("input");
+        input.type = "button";
+        input.id = "p" + i;
+        input.className = "player";
+        playerZone.appendChild(input);
+    }
+    for (let i = 1; i <= 64; i++) {
+        let input = document.createElement("input");
+        input.type = "button";
+        input.id = "c" + i;
+        input.className = "computer";
+        input.setAttribute('onclick', 'game(' + i + ")");
+        computerZone.appendChild(input);
+    }
+    playerZone.style.width = 270;
+    computerZone.style.width = 270;
+}
 }
--- randomThings.js
+++ randomThings.js
@@ -1,131 +1,79 @@
-alert("Jeżeli korzystasz z przęglądarki edge 2.0 zmień przeglądarke dla prawidłowego działania strony.")
-
-let previousPositionC=[];
-let positionP=[];
-let positionC =[];
-if(sizeChoose==1)
-{
-    for(let i=0;i<16;i++)
-    {
-        previousPositionC[i]=i+1;
-        positionP[i]=i+1;
+let previousPositionC = [];
+let positionP = [];
+let positionC = [];
+let active = "";
+let playerPoints = 0;
+let computerPoints = 0;
+let computerShoot = [];
+let cShootNumber = 0;
+let computerMove = 0;
+if (sizeChoose == 1) {
+    for (let i = 0; i < 16; i++) {
+        previousPositionC[i] = i + 1;
+        positionP[i] = i + 1;
     }
-}
-else if(sizeChoose==2)
-{
-    for(let i=0;i<36;i++)
-    {
-        previousPositionC[i]=i+1;
-        positionP[i]=i+1;
+} else if (sizeChoose == 2) {
+    for (let i = 0; i < 36; i++) {
+        previousPositionC[i] = i + 1;
+        positionP[i] = i + 1;
     }
-}
-else if(sizeChoose==3)
-{
-    for(let i=0;i<64;i++)
-    {
-        previousPositionC[i]=i+1;
-        positionP[i]=i+1;
+} else if (sizeChoose == 3) {
+    for (let i = 0; i < 64; i++) {
+        previousPositionC[i] = i + 1;
+        positionP[i] = i + 1;
     }
 }
 
-function randomC()
-{ //tworzenie statków komputera
-    for(let i =0;i<(positionP.length/4);i++ )
-    {
-    let randomIndex=Math.floor(Math.random() * previousPositionC.length);//losowanie
-    let resultC= previousPositionC[randomIndex]; //wpisywanie do tablicy
-        if(!positionC.includes(resultC)) //sprawdzanie czy nie powtarza się
-        {
-        positionC.push(resultC); // wrzucanie do tablicy
-        }
-        else
-        {
-        i--;
+function randomC() { //creating computer Ships
+    for (let i = 0; i < (positionP.length / 4); i++) {
+        let randomIndex = Math.floor(Math.random() * previousPositionC.length);
+        let resultC = previousPositionC[randomIndex];
+        if (!positionC.includes(resultC)) {
+            positionC.push(resultC);
+        } else {
+            i--;
         }
     }
-    for(let j=0;j<(positionP.length/4);j++)
-    {
-    document.getElementById('c'+positionC[j]).className = "computerShip"; //tworzenie statków
+    for (let j = 0; j < (positionP.length / 4); j++) {
+        document.getElementById('c' + positionC[j]).className = "computerShip"; //create ship
     }
 }
-let active ="";
-let x=0;//punkty "zwycięstwa" gracza
-let j=0;//punkty "zwycięstwa" komputera
-let computerShoot =[]; //strzały komputera
-let cShootNumber =0; // indeks do strzałów komputera
-let computerMove =0; // ruch komputera
-function game(numer)
-{ 
-    //ruch gracza
-    active= document.getElementById("c"+numer);
-    //console.log(aktywna);
-    active.value="x";
-    active.setAttribute('onclick','console.log("already shot")');
-    if(active.className=="computerShip") // czy gracz trafił w statek komputera
-    {
-        //console.log("gracz trafił")
-        active.className="destroyedComputerShip";
-        x++;
+
+function game(number) {
+    active = document.getElementById("c" + number);
+    active.setAttribute('onclick', 'console.log("already shot")');
+    active.style.background = "cyan";
+    if (active.className == "computerShip") {
+        active.className = "destroyedComputerShip";
+        active.style.background = "black";
+        playerPoints += 1;
     }
     //ruch komputera
-    else
-    { 
-    computerMove = Math.floor(Math.random()*positionP.length);
-    while (computerShoot.includes("p"+(computerMove+1)))// czy strzał sie powtórzył
-    {
-        computerMove = Math.floor(Math.random()*positionP.length);
-    }
-        computerShoot[cShootNumber] = "p"+(computerMove+1);// zapisywanie strzału komputera
-        cShootNumber+=1;
-        document.getElementById("p"+(computerMove+1)).value = "x";
-        while(document.getElementById("p"+(computerMove+1)).className == "playerShip")
-        {
-            // console.log("pierwsze z rzedu trafienie komputera")
-            document.getElementById("p"+(computerMove+1)).className = "destroyedPlayerShip"
-            computerShoot[cShootNumber] = "p"+(computerMove+1);// zapisywanie strzału komputera
-            cShootNumber+=1;
-            computerMove = Math.floor(Math.random()*positionP.length);
-            document.getElementById("p"+(computerMove+1)).value = 'x';
-            j=j+1;
-            console.log(j);
-            // if(document.getElementById("p"+(computerMove+1)).className == "playerShip")
-            // {
-            //     console.log("drugie z rzedu trafienie komputera")
-            //     document.getElementById("p"+(computerMove+1)).className = "destroyedPlayerShip"
-            //     computerShoot[cShootNumber] = "p"+(computerMove+1);// zapisywanie strzału komputera
-            //     cShootNumber+=1;
-            //     computerMove = Math.floor(Math.random()*positionP.length);
-            //     j=j+1;
-            //     document.getElementById("p"+(computerMove+1)).value = 'x';
-            //     if(document.getElementById("p"+(computerMove+1)).className == "playerShip")
-            //     {
-            //         console.log("trzeciev trafienie komputera")
-            //         document.getElementById("p"+(computerMove+1)).className = "destroyedPlayerShip"
-            //         computerShoot[cShootNumber] = "p"+(computerMove+1);// zapisywanie strzału komputera
-            //         cShootNumber+=1;
-            //         computerMove = Math.floor(Math.random()*positionP.length);
-            //         j=j+1;
-            //         document.getElementById("p"+(computerMove+1)).value = 'x';
-            //         if(document.getElementById("p"+(computerMove+1)).className == "playerShip")
-            //         {
-            //             document.getElementById("p"+(computerMove+1)).className = "destroyedPlayerShip"
-            //             console.log("czwarte z rzedu trafienie komputera")
-            //             j=j+1;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-            //         }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-            //     }
-            // }
+    else {
+        computerMove = Math.floor(Math.random() * positionP.length); //shoot
+        while (computerShoot.includes("p" + (computerMove + 1))) {
+            computerMove = Math.floor(Math.random() * positionP.length); //second shoot if already shot
         }
+        computerShoot[cShootNumber] = "p" + (computerMove + 1);
+        cShootNumber += 1;
+        console.log(document.getElementById("p" + (computerMove + 1)))
+        while (document.getElementById("p" + (computerMove + 1)).className == "playerShip") {
+            document.getElementById("p" + (computerMove + 1)).className = "destroyedPlayerShip";
+            computerMove = Math.floor(Math.random() * positionP.length);
+            console.log(document.getElementById("p" + (computerMove + 1)))
+            computerShoot[cShootNumber] = "p" + (computerMove + 1);
+            cShootNumber += 1;
+            computerPoints += 1;
+        }
+        document.getElementById("p" + (computerMove + 1)).style.background = "cyan";
     }
-    if (x==(positionP.length/4))
-    {
+    if (playerPoints == (positionP.length / 4)) {
         alert("gracz wygrał")
         location.reload();
     }
-    if (j==(positionP.length/4))
-    {
+    if (computerPoints == (positionP.length / 4)) {
         alert("komputer wygrał")
         location.reload();
     }
 }
 randomC();
-//PRZEROBIĆ STRZAŁY KOMPUTRA NA NIESKOŃCZONĄ ILOŚĆ STRZAŁÓW
